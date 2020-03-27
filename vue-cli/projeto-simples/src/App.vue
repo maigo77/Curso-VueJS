7a8b3cf4ff71e9db08d5086bc80a27ec0209931c
maigo77 <38216218+maigo77@users.noreply.github.com>
2020-03-27 11:17:59 -0300

    Comencando secao com vue-cli

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app"> 
+    {{ titulo }}
+    <button @click="titulo += '#'"> Alterar </button>
+  </div>
+</template>
+
+<script>
+   
+  export default {
+    data: () => {
+      return {
+        titulo: 'Ola Vue!!!'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app{
+    background: chocolate;
+    color: #FFF
+  }
+</style>
