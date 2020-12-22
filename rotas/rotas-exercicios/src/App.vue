35d05afe4d3c592d27a093aa3439ece233b4cc8b
Augusto <augusto.seabra@uranet.com.br>
2020-12-22 16:23:20 -0300

    iniciando capitulo de rotas

---
+++ rotas/rotas-exercicios/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="app">
+		<h1>Rotas com VueRouter</h1>
+		<Menu />
+		<router-view />
+	</div>
+</template>
+
+<script>
+import Menu from "./components/template/Menu"
+
+export default {
+	components: {
+		Menu
+	}
+}
+</script>
+
+<style>
+	#app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
+
+<!--
+	Hash -> localhost:8080/#/
+	History -> localhost:8080/...
+
+	Modo hash -> req -> index.html (referencia para o app.js - VUE), entao no caso quando eh passado uma rota /usuarios por exemplo, nao se faz uma requisicao para o servidor, pois eh interpretado apenas pelo router 
+	para fazer a renderizacao
+
+	Modo history -> req (/usuario) o que sera enviado para o servidor eh /usuario (caso a rota nao esteja implementada, nao passara pelo index.html e no caso nao iria renderizar nada)
+-->
---
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="menu">
+    <ul>
+      <router-link to="/" tag="li"><a>Inicio</a></router-link>
+      <router-link to="/usuario" tag="li"><a>Usuario</a></router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
